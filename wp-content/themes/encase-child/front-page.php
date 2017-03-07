e57d56987c8f2c34a28ed9e06e1f45139cac0acb
Kathy Hoile <kohoile@outlook.com>
2017-03-07 11:46:45 -0500

    trying to fix image problems

--- wp-content/themes/encase-child/front-page.php
+++ wp-content/themes/encase-child/front-page.php
@@ -15,11 +15,8 @@
 get_header(); ?>
 <section class="home-page">
 	<div class="site-content">
-		<?php while ( have_posts() ) : the_post(); ?>
 			<div class="homepage-hero">
-				<?php the_content(); ?>
 			</div><!-- .homepage-hero -->
-		<?php endwhile; // end of the loop. ?>
 	</div><!-- .site-content -->
 </section><!-- .home-page -->
 	
